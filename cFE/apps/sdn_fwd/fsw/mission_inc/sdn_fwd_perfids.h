ce6838db8cdbf099a1aa5bc3d5f392f2bbf7d029
None <None>
2019-03-23 15:39:16 -0700

    TO and CI openflow to/from ccsds packets

---
+++ cFE/apps/sdn_fwd/fsw/mission_inc/sdn_fwd_perfids.h
@@ -0,0 +1,21 @@
+/************************************************************************
+** File:
+**   $Id: sdn_fwd_perfids.h  $
+**
+** Purpose: 
+**  Define SDN FWD Performance IDs
+**
+** Notes:
+**
+*************************************************************************/
+#ifndef _sdn_fwd_perfids_h_
+#define _sdn_fwd_perfids_h_
+
+
+#define SDN_FWD_PERF_ID              91 
+
+#endif /* _sdn_fwd_perfids_h_ */
+
+/************************/
+/*  End of File Comment */
+/************************/
---
+++ cFE/apps/sdn_fwd/fsw/platform_inc/sdn_fwd_msgids.h
@@ -0,0 +1,23 @@
+/************************************************************************
+** File:
+**   $Id: sdn_fwd_msgids.h  $
+**
+** Purpose: 
+**  Define SDN FWD  Message IDs
+**
+** Notes:
+**
+**
+*************************************************************************/
+#ifndef _sdn_fwd_msgids_h_
+#define _sdn_fwd_msgids_h_
+
+#define SDN_FWD_CMD_MID            	0x1882
+#define SDN_FWD_SEND_HK_MID        	0x1883
+#define SDN_FWD_HK_TLM_MID		0x0883
+
+#endif /* _sdn_fwd_msgids_h_ */
+
+/************************/
+/*  End of File Comment */
+/************************/
